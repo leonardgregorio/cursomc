e0d2bb26dd9d2c2e05b3286992ae5722d8f78073
Leonardo Gregorio <leonard.gregorio@gmail.com>
2018-08-04 12:15:25 -0300

    Testando primeira classe categoria

--- src/main/java/com/araleo02/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/araleo02/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.araleo02.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.araleo02.cursomc.domain.Categoria;
 
 @RestController // anotacao para indicar uma classe rest
 @RequestMapping(value = "/categorias") // o rest vai responder por esse nome
 public class CategoriaResource { // classe rest - fica em resource
 
 	@RequestMapping(method = RequestMethod.GET) // para obter um dado, usa GET
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<>();
+
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
